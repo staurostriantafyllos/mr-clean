e743df58be628ce7d0f25e48e72b54619a9450b5
Stauros Triantafyllos <staurostriantafyllos@icloud.com>
2025-01-27 17:13:41 +0200

    Split database engine creation in separate method

--- be_task_ca/database.py
+++ be_task_ca/database.py
@@ -9,20 +9,24 @@ SessionLocal = None
 
 Base = declarative_base()
 
-
-def get_db_session():
-
-    database_config = DatabaseSettings()
-
-    global engine, SessionLocal
+def get_db_engine():
+    global engine
     if not engine:
+        database_config = DatabaseSettings()
         engine = create_engine(database_config.CONNECTION_STRING)
 
+    return engine
+
+
+def get_db_session():
+    database_config = DatabaseSettings()
+
+    global SessionLocal
     if not SessionLocal:
         SessionLocal = sessionmaker(
             autocommit=database_config.AUTOCOMMIT,
             autoflush=database_config.AUTOFLUSH,
-            bind=engine,
+            bind=get_db_engine(),
         )
 
     session = SessionLocal()
