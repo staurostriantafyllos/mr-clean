01b946c746c17f1cd99906ee1a2d4436f241211b
Stauros Triantafyllos <staurostriantafyllos@icloud.com>
2025-02-02 03:11:34 +0200

    Add authentication via Keycloak

---
+++ be_task_ca/auth/api.py
@@ -0,0 +1,54 @@
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import JSONResponse
+
+from be_task_ca.auth.repository import (
+    create_user,
+    login_user,
+    refresh_token,
+)
+from be_task_ca.auth.schema import (
+    RefreshRequest,
+    TokenResponse,
+    UserCredentials,
+    UserSignup,
+)
+from be_task_ca.exceptions import (
+    UserLoginError,
+    UserSignupError,
+    UserTokenRefreshException,
+)
+
+auth_router = APIRouter(
+    prefix="/auth",
+    tags=["auth"],
+)
+
+
+@auth_router.post("/signup")
+async def signup(user: UserSignup):
+    try:
+        status_code = await create_user(user)
+    except UserSignupError as e:
+        raise HTTPException(status_code=e.status_code, detail=e.message)
+
+    return JSONResponse(status_code=status_code, content={"message": "User created"})
+
+
+@auth_router.post("/login", response_model=TokenResponse)
+async def login(credentials: UserCredentials):
+    try:
+        status_code, data = await login_user(credentials)
+    except UserLoginError as e:
+        raise HTTPException(status_code=e.status_code, detail=e.message)
+
+    return JSONResponse(status_code=status_code, content=data)
+
+
+@auth_router.post("/refresh", response_model=TokenResponse)
+async def refresh(request: RefreshRequest):
+    try:
+        status_code, data = await refresh_token(request.refresh_token)
+    except UserTokenRefreshException as e:
+        raise HTTPException(status_code=e.status_code, detail=e.message)
+
+    return JSONResponse(status_code=status_code, content=data)
--- be_task_ca/exceptions.py
+++ be_task_ca/exceptions.py
@@ -1,3 +1,6 @@
+import json
+
+
 class UserAlreadyExistsError(Exception):
     pass
 
@@ -19,4 +22,38 @@ class ItemQuantityError(Exception):
 
 
 class ItemAlreadyInCartError(Exception):
+    pass
+
+
+class TokenExpiredError(Exception):
+    pass
+
+
+class TokenMalformedError(Exception):
+    pass
+
+
+class TokenInvalidError(Exception):
+    pass
+
+
+class UserException(Exception):
+    def __init__(self, status_code: int, message: str):
+        self.status_code = status_code
+        try:
+            self.message = json.loads(message)  # Try parsing as JSON
+        except (json.JSONDecodeError, TypeError):
+            self.message = {"error": message}
+        super().__init__(message)
+
+
+class UserSignupError(UserException):
+    pass
+
+
+class UserLoginError(UserException):
+    pass
+
+
+class UserTokenRefreshException(UserException):
     pass
